89c99bd66f9ff6a234177ec67fded7fe1543407d
Esteban Pastorino <ejpastorino@gmail.com>
2018-12-27 11:26:58 +0100

    Use exit code 78 instead of custom ignore file

--- diff-includes/entrypoint.sh
+++ diff-includes/entrypoint.sh
@@ -4,5 +4,5 @@
 if ! git diff-index --quiet HEAD~$(jq '.commits | length' "${GITHUB_EVENT_PATH}") $*; then
   echo "Changes in $*, proceeding"
 else
-  echo "No changes in $*, stopping" && echo "ignore:$*" >> "$HOME/ignore"
+  echo "No changes in $*, stopping" && exit 78
 fi
