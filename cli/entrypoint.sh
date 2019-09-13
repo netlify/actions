296bf61998720bbe96bbf32fd441b5fba4e8535b
Ivan Josipovic <9521987+IvanJosipovic@users.noreply.github.com>
2019-09-13 14:33:13 -0700

    CLI output is saved to variable called output
    CLI output is saved to variable called output

--- cli/entrypoint.sh
+++ cli/entrypoint.sh
@@ -1,3 +1,7 @@
 #!/bin/sh -l
 
-sh -c "if grep \"^ignore:$BUILD_DIR\" $HOME/ignore; then ls $HOME && exit 0; else ${BUILD_COMMAND:-echo} && netlify $*; fi"
+if grep "^ignore:$BUILD_DIR" $HOME/ignore; then
+ ls $HOME && exit 0;
+else 
+ ${BUILD_COMMAND:-echo} && output=$(netlify $*); echo ::set-output name=output::$output;
+fi
