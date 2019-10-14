40541f11d0a771f885cf7260172491d7b280b4a2
Antonio Britto <antonioabritto@gmail.com>
2019-10-14 14:30:56 -0300

    refactor entrypoint command as "shell -c" in order to enabled redirecting of output/pipeing

--- cli/entrypoint.sh
+++ cli/entrypoint.sh
@@ -1,7 +1,11 @@
 #!/bin/sh -l
 
-if [ -f "$HOME/ignore" ] && grep "^ignore:$BUILD_DIR" "$HOME/ignore"; then
-  echo "$BUILD_DIR didn't change"
-else
-  ${BUILD_COMMAND:-echo} && netlify $*
-fi
+read -d '' COMMAND <<- EOF
+  if [ -f "$HOME/ignore" ] && grep "^ignore:$BUILD_DIR" "$HOME/ignore"; then
+    echo "$BUILD_DIR didn't change"
+  else
+    ${BUILD_COMMAND:-echo} && netlify $*
+  fi
+EOF
+
+sh -c "$COMMAND"
