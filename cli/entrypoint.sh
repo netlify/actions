e1ecbc750f5114222fedb25fb8c4f0d4e18c8b7a
Patryk Padus <patryk.padus@smartmage.pl>
2020-02-27 13:32:48 +0100

    Output to variable. Update node version. Special env to handle urls

--- cli/entrypoint.sh
+++ cli/entrypoint.sh
@@ -1,7 +1,21 @@
 #!/bin/sh -l
 
-if [ -f "$HOME/ignore" ] && grep "^ignore:$BUILD_DIR" "$HOME/ignore"; then
-  echo "$BUILD_DIR didn't change"
-else
-  ${BUILD_COMMAND:-echo} && netlify $*
-fi
+read -d '' COMMAND <<- EOF
+  if [ -f "$HOME/ignore" ] && grep "^ignore:$BUILD_DIR" "$HOME/ignore"; then
+    echo "$BUILD_DIR didn't change"
+  else
+    ${BUILD_COMMAND:-echo} && netlify $*
+  fi
+EOF
+
+OUTPUT=$(sh -c "$COMMAND")
+
+NETLIFY_OUTPUT=$(echo "$OUTPUT")
+NETLIFY_URL=$(echo "$OUTPUT" | grep -Eo '(http|https)://[a-zA-Z0-9./?=_-]*(--)[a-zA-Z0-9./?=_-]*') #Unique key: --
+NETLIFY_LOGS_URL=$(echo "$OUTPUT" | grep -Eo '(http|https)://app.netlify.com/[a-zA-Z0-9./?=_-]*') #Unique key: app.netlify.com
+NETLIFY_LIVE_URL=$(echo "$OUTPUT" | grep -Eo '(http|https)://(?!netlify.com)[a-zA-Z0-9./?=_-]*' | grep -Eov "netlify.com") #Unique key: don't containr -- and app.netlify.com
+
+echo "::set-output name=NETLIFY_OUTPUT::$NETLIFY_OUTPUT"
+echo "::set-output name=NETLIFY_URL::$NETLIFY_URL"
+echo "::set-output name=NETLIFY_LOGS_URL::$NETLIFY_LOGS_URL"
+echo "::set-output name=NETLIFY_LIVE_URL::$NETLIFY_LIVE_URL"
