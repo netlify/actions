14ff582c3f323b421b6e320b4927594c4967d6a0
Praise-God Emerenini <praisegeek@gmail.com>
2020-06-06 15:44:23 +0100

    Add exit code to halt workflow
    Exit workflow from jobs that needs it as a dependency

--- diff-includes/entrypoint.sh
+++ diff-includes/entrypoint.sh
@@ -4,5 +4,6 @@
 if ! git diff-index --quiet HEAD~$(jq '.commits | length' "${GITHUB_EVENT_PATH}") $*; then
   echo "Changes in $*, proceeding"
 else
-  echo "No changes in $*, stopping" && echo "ignore:$*" >> "$HOME/ignore"
+  echo "No changes in $*, stopping" && echo exit 1;
+  # echo "No changes in $*, stopping" && echo "ignore:$*" >> "$HOME/ignore"
 fi
