e49192e75936a0ca8f1eca25dc4ad58ab12aee6e
Jeffrey Aven <javen@avensolutions.com>
2025-05-20 23:35:18 +1000

    Update entrypoint.sh

--- cli/entrypoint.sh
+++ cli/entrypoint.sh
@@ -15,10 +15,9 @@ NETLIFY_URL=$(echo "$OUTPUT" | grep -Eo '(http|https)://[a-zA-Z0-9./?=_-]*(--)[a
 NETLIFY_LOGS_URL=$(echo "$OUTPUT" | grep -Eo '(http|https)://app.netlify.com/[a-zA-Z0-9./?=_-]*') #Unique key: app.netlify.com
 NETLIFY_LIVE_URL=$(echo "$OUTPUT" | grep -Eo '(http|https)://[a-zA-Z0-9./?=_-]*' | grep -Eov "netlify.com") #Unique key: don't containr -- and app.netlify.com
 
-{
-  echo "NETLIFY_OUTPUT=$NETLIFY_OUTPUT"
-  echo "NETLIFY_URL=$NETLIFY_URL"
-  echo "NETLIFY_LOGS_URL=$NETLIFY_LOGS_URL"
-  echo "NETLIFY_LIVE_URL=$NETLIFY_LIVE_URL"
-} >> "$GITHUB_OUTPUT"
+[ -n "$NETLIFY_OUTPUT" ] && echo "NETLIFY_OUTPUT=$NETLIFY_OUTPUT" >> "$GITHUB_OUTPUT"
+[ -n "$NETLIFY_URL" ] && echo "NETLIFY_URL=$NETLIFY_URL" >> "$GITHUB_OUTPUT"
+[ -n "$NETLIFY_LOGS_URL" ] && echo "NETLIFY_LOGS_URL=$NETLIFY_LOGS_URL" >> "$GITHUB_OUTPUT"
+[ -n "$NETLIFY_LIVE_URL" ] && echo "NETLIFY_LIVE_URL=$NETLIFY_LIVE_URL" >> "$GITHUB_OUTPUT"
+
 
